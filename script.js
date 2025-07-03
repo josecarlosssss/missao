22e3b88c859c70261ffaa1efe1994f509382ce9a
josecarlosssss <160252178+josecarlosssss@users.noreply.github.com>
2025-07-03 11:17:12 -0300

    opa

---
+++ script.js
@@ -0,0 +1,3 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergustas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
